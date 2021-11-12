a35455fe1d4435439a0d0f0cf938d01597395661
jawad khan <73393333+jkbells@users.noreply.github.com>
2021-11-12 20:49:41 +0500

    week1 codes

---
+++ Week-1/Swing.java
@@ -0,0 +1,30 @@
+
+import javax.swing.*; 
+
+public class Swing {
+
+	public static void main(String args[]){
+		
+	String input1 = JOptionPane.showInputDialog("Enter first Number");
+	double number1 = Double.parseDouble(input1);
+	String input2 = JOptionPane.showInputDialog("Enter Second Number");
+	double number2 = Double.parseDouble(input2);
+	double sum = number1 + number2; 
+	
+	JOptionPane.showMessageDialog(null, "The sum of two numbe
+import javax.swing.*; 
+
+public class Swing {
+
+	public static void main(String args[]){
+		
+	String input1 = JOptionPane.showInputDialog("Enter first Number");
+	double number1 = Double.parseDouble(input1);
+	String input2 = JOptionPane.showInputDialog("Enter Second Number");
+	double number2 = Double.parseDouble(input2);
+	double sum = number1 + number2; 
+	
+	JOptionPane.showMessageDialog(null, "The sum of two numbers = "+ sum);
+	}
+	
+ }
